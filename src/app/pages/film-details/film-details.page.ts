0310ecc5b10d14cbb0689bb3023a963734f56303
Jose <jeal.code47@gmail.com>
2020-01-20 16:16:01 -0300

    favorite film

--- src/app/pages/film-details/film-details.page.ts
+++ src/app/pages/film-details/film-details.page.ts
@@ -1,3 +1,4 @@
+import { FavoriteService } from './../../services/favorite.service';
 import { ApiService } from './../../services/api.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -10,13 +11,31 @@ import { ActivatedRoute } from '@angular/router';
 export class FilmDetailsPage implements OnInit {
 
   film: any;
+  isFavorite = false;
+  filmId = null;
 
-  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService, private favoriteService: FavoriteService) { }
 
   ngOnInit() {
     let id = this.activatedRoute.snapshot.paramMap.get('id');
     this.api.getFilm(id).subscribe(res => {
       this.film = res;
+    });
+
+    this.favoriteService.isFavorite(this.filmId).then(isFav => {
+      this.isFavorite = isFav;
+    });
+  }
+
+  favoriteFilm() {
+    this.favoriteService.favoriteFilm(this.filmId).then(() => {
+      this.isFavorite = true;
+    });
+  }
+
+  unfavoriteFilm() {
+    this.favoriteService.unfavoriteFilm(this.filmId).then(() => {
+      this.isFavorite = false;
     });
   }
 
